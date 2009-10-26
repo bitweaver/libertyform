2e89651a06c33c14173cb7792a071741d49199cd
Daniel Sutcliffe <dansut@users.sourceforge.net>
2009-10-26 19:33:59 +0000

    Deal with options fields having no or one option better

--- smarty/function.formfields.php
+++ smarty/function.formfields.php
@@ -240,6 +240,7 @@ function smarty_function_formfields($params, &$gBitSmarty) {
 				$forminput .= '<input type="hidden" name="'.$grpname.'['.$fieldname.'][lastindex]" id="'.$fieldname.'_lastindex"
 					value="'.$idx.'" />';
 				$forminput .= '</table>';
+				if(empty($newinprow) && empty($field['value'])) $forminput = "<em>Sorry, no options available right now!</em>";
 				break;
 			case 'text':
 			default:
@@ -307,14 +308,23 @@ function optionsInput($sparams, $field, &$smarty) {
 
 	$realopts = 0;
 	$optgroups = FALSE;
+	// These 2 below are only used in the case of 1 real option ...
+	$savekey = 0;
+	$saveval = '';
 	if(isset($sparams['options'])) foreach($sparams['options'] as $key => $val) {
 		if(is_array($val)) { // Deal with optgroup hierarchy
-			$optgroups = FALSE;
+			$optgroups = TRUE;
+			$saveval = $key;
 			foreach($val as $subkey => $subval) {
-				if(!empty($subkey)) $realopts++;
+				if(!empty($subkey)) {
+					$realopts++;
+					$saveval .= ', '.$subval;
+					$savekey = $subkey;
+				}
 			}
 		} else if(!empty($key)) {
 			$realopts++;
+			$savekey = $key;
 		}
 		if($realopts > 1) break; // Currently don't care if any more than 1 real option so don't waste time
 	}
@@ -323,18 +333,19 @@ function optionsInput($sparams, $field, &$smarty) {
 	   !(isset($field['shownullopt']) && array_key_exists(0, $sparams['options']))) { // no NULL option we want to show
 		return "";
 	} elseif((($realopts == 1) && !isset($field['shownullopt'])) || // Only 1 (real) option and not showing null option, or
-	         (isset($field['createonly']) && ($field['createonly'] == TRUE))) { // this is a 'create only' (no edit) field
+	         (isset($field['createonly']) && $field['createonly'] && !empty($sparams['selected']))) { // 'create only' field
 		// Don't bother with the dropdown, just show text and a hidden field for value.
-		// Shouldn't ever really use this display value, but if can't get good text it defaults to this
-		$display = '<em>optionval=</em>&quot;'.$sparams['selected'].'&quot;';
+		$selected = (empty($sparams['selected'])? $savekey : $sparams['selected']); // if nothing selected use only value
 		if(isset($field['displayfunc']) && is_callable($field['displayfunc'])) {
-			$display = call_user_func($field['displayfunc'], $sparams['selected']);
+			$display = call_user_func($field['displayfunc'], $selected);
 		} elseif($optgroups) { // The selections are organized in hierarchical optgroups
-			$display .= '<strong>optgroup</strong>';
-		} elseif(isset($field['options'][$sparams['selected']])) { // Regular option display
-			$display = $field['options'][$sparams['selected']];
+			$display = $saveval; // This was set up earlier when we looped through options.
+		} elseif(isset($field['options'][$selected])) { // Regular option display
+			$display = $field['options'][$selected];
+		} else { // Shouldn't ever really use this default display value, but if can't get other good text
+			$display = 'No displayable val, here\'s the Id: <em>optionval=</em>&quot;'.$selected.'&quot;';
 		}
-		return $display.'<input type="hidden" name="'.$sparams['name'].'" id="'.$sparams['id'].'" value="'.$sparams['selected'].'" />';
+		return $display.'<input type="hidden" name="'.$sparams['name'].'" id="'.$sparams['id'].'" value="'.$selected.'" />';
 	} else { // normal drop down option processing
 		require_once($smarty->_get_plugin_filepath('function', 'html_options'));
 		return smarty_function_html_options($sparams, $smarty);
