952d5f9b0a67d158561f07c53cf16be8a08b7de7
Daniel Sutcliffe <dansut@users.sourceforge.net>
2009-12-30 22:08:11 +0000

    Introduce a Smarty modifier for currency output, initially very limited

---
+++ smarty/modifier.currency_format.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package libertyform
+ * @subpackage smarty
+ */
+
+/**
+ * Basic function that initially will just convert an integer number of cnets into a dallar and cents string
+ * 
+ * @param int $pAmount
+ * @access public
+ * @return converted string on success, FALSE on failure
+ */
+function smarty_modifier_currency_format($pAmount) {
+	if(!is_numeric($pAmount)) return '\$invalid';
+	$amount = (int)$pAmount;
+	$ret = ($amount > 0) ? '' : '-';
+	$ret .= '$'.((int)($amount/100)).'.'.sprintf('%02u', $amount%100);
+	return $ret;
+}
+?>
