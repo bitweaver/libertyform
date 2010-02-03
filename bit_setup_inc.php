893a48d31b87c183d2f95d284be9c5eb8c93d176
Daniel Sutcliffe <dansut@users.sourceforge.net>
2010-02-03 21:32:22 +0000

    Move libertyform css to standardized place

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -11,6 +11,6 @@ $gBitSystem->registerPackage($registerHash);
 // Add our Smarty plugin directory.
 $gBitSmarty->plugins_dir[] = $registerHash['package_path']."smarty";
 
-$gBitThemes->loadCss(LIBERTYFORM_PKG_PATH.'bit_pkgstyle.css');
+$gBitThemes->loadCss(LIBERTYFORM_PKG_PATH.'css/libertyform.css');
 
 ?>
